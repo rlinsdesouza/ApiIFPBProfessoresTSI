51a003a670b8966c51b8f7154df267e15a99f5c7
rlinsdesouza <rlinsdesouza@gmail.com>
2019-05-11 17:22:01 -0300

    api v1

--- ApiIFPBProfessoresTSI/api/models.py
+++ ApiIFPBProfessoresTSI/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Professor (models.Model) :
     nome = models.CharField(max_length=100, blank=False)
-    frasemarcante = models.CharField(max_length=100)
+    frasemarcante = models.CharField(max_length=100, blank=True, null=True)
     foto = models.ImageField(
         # upload_to=None,
         # height_field=None,
