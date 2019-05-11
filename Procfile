ae4e17007b56d94557be4d90d7644e4615eb6d9d
rlinsdesouza <rlinsdesouza@gmail.com>
2019-05-11 14:52:16 -0300

    add django-heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app --log-file=-
+web: gunicorn apiprofessoresifpbtsi:app --log-file=-
--- apiProfIFPBTSI.py
+++ apiProfIFPBTSI.py
@@ -1,8 +1,23 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
 
-def hello() :
-    return 'Hello World!'
+# # def hello() :
+# #     return 'Hello World!'
+
+
+# [[source]]
+# url = "https://pypi.python.org/simple"
+# verify_ssl = true
+
+# [packages]
+# dj-database-url = "*"
+# django = "*"
+# gunicorn = "*"
+# psycopg2 = "*"
+# whitenoise = "*"
+
+# [requires]
+# python_version = "3.4.3"
