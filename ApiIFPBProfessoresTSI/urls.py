3cc5d06cfc4d1a30209067916eac804c75ad83cf
rlinsdesouza <rlinsdesouza@gmail.com>
2019-05-11 16:09:28 -0300

    correcao exibicao media

--- ApiIFPBProfessoresTSI/urls.py
+++ ApiIFPBProfessoresTSI/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 from .api import views
 
 router = routers.DefaultRouter()
@@ -28,4 +30,4 @@ urlpatterns = [
 ] 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
